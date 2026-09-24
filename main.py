6c3ea481ed73f5992bac41479702bdd4dcd5e7da
Audioflow-net <besse.genever@googlemail.com>
2026-09-24 11:16:08 +0200

    style: use the user-provided transparent Final Logo InEar Snitch.png

--- main.py
+++ main.py
@@ -825,7 +825,7 @@ class MainWindow(QMainWindow):
         from PySide6.QtCore import Qt
         
         logo_img = QLabel()
-        logo_path = os.path.join(os.path.dirname(__file__), "Final_Logo_Spy_Cleaned.svg")
+        logo_path = os.path.join(os.path.dirname(__file__), "Final Logo InEar Snitch.png")
         if os.path.exists(logo_path):
             pix = QPixmap(logo_path)
             if not pix.isNull():
